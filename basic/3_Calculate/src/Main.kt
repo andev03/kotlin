2c7e1e24009d2d6115c1acc8300cd83d43232769
andev03 <nguyenhoangan03.work@gmail.com>
2024-09-06 22:41:05 +0700

    kotlin basic

---
+++ basic/3_Calculate/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var a = 10 //khai báo tắt, tự động nhận kiểu int
+    var b = 3 //khai báo tắt, tự động nhận kiểu int
+
+    println(a::class.java.typeName)
+    println(a::class.java.typeName)
+    // phéo cộng
+    println("Cách 1 a + b = ")
+
+}
